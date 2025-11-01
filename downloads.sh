6ebbcd65d11ca2674e66ec711c39bf0b3d3aac10
휘래 김 <hramst0618@gmail.com>
2025-11-01 23:02:32 +0900

    Hive Metastore 연결을 위한 Jar 파일 다운로드

---
+++ downloads.sh
@@ -0,0 +1,16 @@
+DOWN_DIR=/home/ec2-user/downloads
+cd ${DOWN_DIR}
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-beeline/3.1.2/hive-beeline-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-cli/3.1.2/hive-cli-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-common/3.1.2/hive-common-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-exec/3.1.2/hive-exec-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-jdbc/3.1.2/hive-jdbc-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-llap-common/3.1.2/hive-llap-common-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-metastore/3.1.2/hive-metastore-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-serde/3.1.2/hive-serde-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/shims/hive-shims-0.23/3.1.2/hive-shims-0.23-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-shims/3.1.2/hive-shims-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/shims/hive-shims-common/3.1.2/hive-shims-common-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/shims/hive-shims-scheduler/3.1.2/hive-shims-scheduler-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/hive/hive-standalone-metastore/3.1.2/hive-standalone-metastore-3.1.2.jar
+curl -O https://repo1.maven.org/maven2/org/apache/calcite/calcite-core/1.16.0/calcite-core-1.16.0.jar
